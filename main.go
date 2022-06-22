8c9e3dbdd492d33af027c98da7aee3ffa158a20e
Xuan Huy NGUYEN <nxhuy2611@gmail.com>
2022-06-22 21:38:25 +0200

    Go is a typed programmin language

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ import "fmt"
 // meaning that for ONE Go application, we only need ONE MAIN function bc we only have ONE ENTRYPOINT
 func main() {
 	// declare a variable
+	// when we declare a variable and assign it a value immediately, Go can IMPLY its data-type
 	var conferenceName = "Go Conference"
 	// like var but cannot be changed
 	const conferenceTickets = 50
 	var remainingTickets = 50
+
+	// fmt.Printf("conferenceName is %T, remainingTickets is %T, conferenceTickets is %T\n", conferenceName, remainingTickets, conferenceTickets)
 
 	// fmt.Println("Welcome to", conferenceName, "booking application")
 	// fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
@@ -23,5 +26,14 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n" , conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	// Go is statically TYPED LANGUAGE
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
 
 }
