f6cbe5fe9a25241dfbb32345c8e616d30e08b39e
Xuan Huy NGUYEN <nxhuy2611@gmail.com>
2022-06-22 21:25:57 +0200

    intro var, const and formatted print

--- main.go
+++ main.go
@@ -9,5 +9,19 @@ import "fmt"
 // ENTRYPOINT is a MAIN function
 // meaning that for ONE Go application, we only need ONE MAIN function bc we only have ONE ENTRYPOINT
 func main() {
-	fmt.Println("Hello world")
+	// declare a variable
+	var conferenceName = "Go Conference"
+	// like var but cannot be changed
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	// fmt.Println("Welcome to", conferenceName, "booking application")
+	// fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	// fmt.Println("Get your tickets here to attend")
+
+	// Formatted PRINT
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n" , conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
 }
